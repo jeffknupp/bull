2cc0157ebaa8fc67029bdca250ffa84f292f3015
Jeff Knupp <jknupp@appnexus.com>
2014-01-25 10:21:17 -0500

    Create test for downloading file

--- tests/test_bull.py
+++ tests/test_bull.py
@@ -2,6 +2,7 @@
 
 import unittest
 import uuid
+import os
 
 from flask import current_app
 
@@ -18,6 +19,7 @@ class BullTestCase(unittest.TestCase):
         app.config['SITE_NAME'] = 'www.foo.com'
         app.config['STRIPE_SECRET_KEY'] = 'foo'
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config['FILE_DIRECTORY'] = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0], 'files'))
         with app.app_context():
             db.init_app(current_app)
             db.metadata.create_all(db.engine)
@@ -48,19 +50,30 @@ class BullTestCase(unittest.TestCase):
         assert app.config['STRIPE_PUBLIC_KEY'] in response.data
 
     def test_get_user(self):
+        """Can we retrieve the User instance created in setUp?"""
         with app.app_context():
             user = User.query.get('admin@foo.com')
             assert bcrypt.check_password_hash(user.password, 'password')
 
     def test_get_product(self):
+        """Can we retrieve the Product instance created in setUp?"""
         with app.app_context():
             product = Product.query.get(1)
             assert product is not None
             assert product.name == 'Test Product'
 
     def test_get_purchase(self):
+        """Can we retrieve the Purchase instance created in setUp?"""
         with app.app_context():
             purchase = Purchase.query.get(self.purchase_uuid)
             assert purchase is not None
             assert purchase.product.price == 5.01
             assert purchase.email == 'foo@bar.com'
+
+    def test_download_file(self):
+        """Given an exisitng purchase, does visiting the purchase's url allow us
+        to download the file?."""
+        purchase_url = '/' + self.purchase_uuid
+        response = self.app.get(purchase_url)
+        assert response.data == 'Test content\n'
+        assert response.status_code == 200
